71ca35bcb160c164add40395ada9b33087b348ab
Jhamyllie <milli.santana2017@gmail.com>
2025-05-15 10:59:19 -0300

    Adiciona solução do exercício 1-retornar-medias

--- src/implementacao/1-retornar-medias.ts
+++ src/implementacao/1-retornar-medias.ts
@@ -1,3 +1,19 @@
+import { turma } from "../../listaPreConstruida";
+
 function retornarMedias(){
-    return null
+    let mediasDasAlunas: any = turma.map((aluna) => {
+        // (a + b + c) / 3
+        let somaDasNotas = aluna.prova.p1 + aluna.prova.p2 + aluna.prova.p3;
+        let media: number = somaDasNotas / 3;
+
+        return {
+            nome: aluna.nome,
+            // media: media.toFixed(1)
+            media: Number(media.toFixed(1))
+        }
+    })
+    return mediasDasAlunas;
 }
+
+console.log(turma);
+console.log(retornarMedias());
