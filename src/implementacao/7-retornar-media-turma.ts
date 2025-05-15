f5bbef67322e1ceced6e0b3d7e48ccb4a8d588e2
TiciB <tebezerra@gmail.com>
2025-05-15 11:57:04 -0300

    Exercício Retornar Média Turma com import questão 1 - retornar medias

--- src/implementacao/7-retornar-media-turma.ts
+++ src/implementacao/7-retornar-media-turma.ts
@@ -1,3 +1,15 @@
-function retornarMediaTurma() {
-    return null;
+import { retornarMedias } from "./1-retornar-medias";
+type Aluna = {
+  nome: string;
+  media: number;
+};
+function retornarMediaTurma(): number {
+    const alunas: Aluna[] = retornarMedias();
+
+    const somaDasMedias = alunas.reduce((total, aluna) => total + aluna.media, 0);
+    const mediaTurma = somaDasMedias / alunas.length;
+
+    return mediaTurma;
 }
+
+console.log("Média da turma:", retornarMediaTurma().toFixed(1));
