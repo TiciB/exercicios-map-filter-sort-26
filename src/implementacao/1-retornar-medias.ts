c105b882e0a9e79e6b4884b29b3ff51165134715
Jhamyllie <milli.santana2017@gmail.com>
2025-05-16 10:01:51 -0300

    Refactor: Ajusta a tipagem das funções

--- src/implementacao/1-retornar-medias.ts
+++ src/implementacao/1-retornar-medias.ts
@@ -1,8 +1,7 @@
-// import { type } from "../../listaPreConstruida";
 import { turma } from "../../listaPreConstruida";
 
-export function retornarMedias():{}[]{
-    let mediasDasAlunas: any = turma.map((aluna) => {
+export function retornarMedias():{nome: string, media: number}[]{
+    let mediasDasAlunas = turma.map((aluna) => {
         // (a + b + c) / 3
         let somaDasNotas = aluna.prova.p1 + aluna.prova.p2 + aluna.prova.p3;
         let media: number = somaDasNotas / 3;
--- src/implementacao/2-retornar-aprovadas.ts
+++ src/implementacao/2-retornar-aprovadas.ts
@@ -1,6 +1,6 @@
 import { retornarMedias } from "./1-retornar-medias";
 
-function retornarAprovadas(): {}[] {
+function retornarAprovadas(): {nome: string, media: number}[] {
     let alunasAprovadas = retornarMedias().filter((aluna: any) => aluna.media >= 7);
     return alunasAprovadas;
 }
