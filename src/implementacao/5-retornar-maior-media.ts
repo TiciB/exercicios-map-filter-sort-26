506fc417cbf5ed58da1f202abd73aa2a35154a13
Jhamyllie <milli.santana2017@gmail.com>
2025-05-16 14:04:39 -0300

    Adiciona questão 5-retornar maior média

--- src/implementacao/5-retornar-maior-media.ts
+++ src/implementacao/5-retornar-maior-media.ts
@@ -1,3 +1,9 @@
-function retornarMaiorMedia() {
-    return null;
+import { retornarMedias } from "./1-retornar-medias";
+
+function retornarMaiorMedia(): {nome: string, media: number} {
+    let mediasNaOrdem = retornarMedias().sort((a, b) => a.media - b.media);
+    let maiorMedia = mediasNaOrdem[mediasNaOrdem.length - 1];
+    return maiorMedia;
 }
+
+console.log(retornarMaiorMedia());
