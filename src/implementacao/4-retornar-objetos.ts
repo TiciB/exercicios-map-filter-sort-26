520ed394a83b15256e0aedf3189a4e1248147db4
biizerrah <biizerrah@outlook.com>
2025-05-20 10:52:10 -0300

    Exercícios retornar objetos

--- src/implementacao/4-retornar-objetos.ts
+++ src/implementacao/4-retornar-objetos.ts
@@ -1,3 +1,21 @@
-function retornarObjetos() {
-    return null;
+import { turma } from "../../listaPreConstruida";
+
+type Resultado = {
+  nome: string;
+  media: number;
+  aprovada: boolean;
+};
+
+function retornarObjetos(): Resultado[] {
+  return turma.map(aluna => {
+    const { p1, p2, p3 } = aluna.prova;
+    const media = (p1 + p2 + p3) / 3;
+
+    return {
+      nome: aluna.nome,
+      media: parseFloat(media.toFixed(1)),
+      aprovada: media >= 7
+    };
+  });
 }
+console.log(retornarObjetos())
